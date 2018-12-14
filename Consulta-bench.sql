76a7a14f9b006116970e279aee637c8d156cf35d
yerkoparedes <yparedes@recsachile.cl>
2018-12-14 18:49:07 -0300

    Primeras consultas sql

---
+++ Consulta-bench.sql
@@ -0,0 +1,12 @@
+/*
+--PagoSoles: Corresponde al valor pagado o recuperado (Pagos)
+--CodigoCentral: Corresponde al identificador del cliente (N° Clientes)
+--¿Existe algún código de operación?
+--Segmento: Particular o Pyme. ¿Qué son el resto de los segmentos?
+--ImpagadoSolesAgencia: Considerar como Saldo de la deuda (Masa Impagados Distribuida). ¿Qué es ImpagadoSoles?
+--¿Está el producto en el bench o alguna codificación que lo represente?
+*/
+commit
+desc bench_archivos_bbva;
+SELECT * FROM bench_archivos_bbva;
+copy bench_archivos_bbva from 'C:\Users\yparedes\Documents\Postgres Desarrollos\BENCH_20181130_1.csv' delimiter ';' csv header;
